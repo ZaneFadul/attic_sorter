98cc01c19bc1e38ec7ae6fb827d905283f52a057
ZaneFadul <theboonandthezane@gmail.com>
2020-08-01 16:51:12 -0400

    working create tables

--- attic_sorter.py
+++ attic_sorter.py
@@ -8,10 +8,11 @@ Created on Sat Aug  1 14:02:33 2020
 """
 
 import sqlite3
-import tabulate
+import tkinter as tk
 
 #init Categories
-CATEGORIES = ["sell","donate","garbage"]
+TODOS = [(1,"Sell"), (2,"Donate"), (3,"Garbage"), (4,"Keep")]
+CONDITIONS = [(1,"Poor"), (2,"Fair"), (3,"Good"), (4,"Excellent")]
 
 #setup sqlite3 db
 def create_conn(db_file):
@@ -24,39 +25,69 @@ def create_conn(db_file):
         if conn:
             return conn, conn.cursor()
         
+#check tables
+def tables_exist(cursor):
+    item_table = cursor.execute('''SELECT name 
+                          FROM sqlite_master 
+                          WHERE type='table'
+                          AND name="items"''').fetchall()
+    if len(item_table) == 1:
+        return True
+    return False
+
 #init tables
-def create_tables(cursor):
-    #init items table
-    cursor.execute('''CREATE TABLE items (
+def create_tables(conn, cursor):
+    #init todos table
+    cursor.execute('''CREATE TABLE IF NOT EXISTS todos (
                         id INTEGER PRIMARY KEY,
+                        name TEXT NOT NULL)''')
+    for TODO in TODOS:
+        cursor.execute(f'''INSERT INTO todos VALUES ({TODO[0]},"{TODO[1]}")''')
+        
+    #init types table
+    cursor.execute('''CREATE TABLE IF NOT EXISTS types (
+                        id INTEGER PRIMARY KEY,
+                        name TEXT NOT NULL)''')
+    
+    #init conditions table
+    cursor.execute('''CREATE TABLE IF NOT EXISTS conditions (
+                        id INTEGER PRIMARY KEY,
+                        condition TEXT NOT NULL)''')
+    for CONDITION in CONDITIONS:
+        cursor.execute(f'''INSERT INTO conditions VALUES ({CONDITION[0]},"{CONDITION[1]}")''')
+    
+
+
+    #init items table
+    cursor.execute('''CREATE TABLE IF NOT EXISTS items (
+                        id INTEGER PRIMARY KEY,
+                        name TEXT,
+                        desc TEXT,
+                        todo_key INTEGER NOT NULL,
                         type_key INTEGER NOT NULL,
-                        FOREIGN KEY (type_key)
-                            REFERENCES types (id)
+                        cond_key INTEGER NOT NULL,
+                        FOREIGN KEY (todo_key) REFERENCES todos (id)
                                 ON DELETE CASCADE
                                 ON UPDATE CASCADE,
-                        desc TEXT NOT NULL,
-                        cond TEXT NOT NULL,
-                        cat_key INTEGER NOT NULL,
-                        FOREIGN KEY (cat_key)
-                            REFERENCES categories (id)
+                        FOREIGN KEY (type_key) REFERENCES types (id)
+                                ON DELETE CASCADE
+                                ON UPDATE CASCADE,
+                        FOREIGN KEY (cond_key) REFERENCES conditions (id)
                                 ON DELETE CASCADE
                                 ON UPDATE CASCADE
                         )''')
     
-    #init types table
-    cursor.execute('''CREATE TABLE types (
-                        id INTEGER PRIMARY KEY,
-                        name TEXT NOT NULL )''')
-    
-    #init categories table
-    cursor.execute('''CREATE TABLE categories (
-                        id INTEGER PRIMARY KEY,
-                        name TEXT NOT NULL )''')
-    cursor.execute(f'''INSERT INTO categories (name)
-        VALUES {(CATEGORY) for CATEGORY in CATEGORIES};''')
-        
+    conn.commit()
+
+
 if __name__ == '__main__':
     conn, cursor = create_conn('attic.db')
-    create_tables(cursor)
-    cursor.execute('.tables')
-    conn.close()
+    window = tk.Tk()
+    try:
+        if not tables_exist(cursor):
+            create_tables(conn, cursor)
+        
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
